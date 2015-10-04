0508cc21a550c9224c598a2ef5c9bab963a02071
Damien <michelld@hotmail.com>
2015-10-04 23:21:26 +0200

    Merge remote-tracking branch 'origin/master' Inserted data for hearrate. Inserted csv loader, and loader search. Fixed import from 2 rows from csv file.

--- heartrate.py
+++ heartrate.py
@@ -1,10 +1,31 @@
 from __future__ import division
 __author__ = 'Damien'
 
+import matplotlib.pyplot as plt
 import numpy as np
 import math
 import csv
 
-# Data (Damien)
+# Pre-Data (Damien)
 
-max_heartrate = 188
+max_heartrate = 188 # Max hjerteslag - hoy intensitet
+resting_heartrate = 52 # Hvilepuls - Generell
+
+# csv - file loader
+
+def heartrate_loader(filename):
+    records = []
+    with open(filename, 'rb') as csvfile: # Don't overwrite file.
+        csvreader = csv.reader(csvfile, delimiter = ',') # Distinguishes between types
+        for row in csvreader:
+            if len(row) == 2: # Amount of rows.
+                records.append({
+                    "navn": row[0].strip(),
+                    "heartrate_low": int(row[1].strip())
+                })
+    return records
+
+# Import fil fra mappe
+heartrate_data = heartrate_loader("Data/heartrate.data")
+
+
