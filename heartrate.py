73a2b04aa6324b573919ab7f1cd0e25d9b472181
Damien <michelld@hotmail.com>
2015-10-08 18:30:35 +0200

    Removed some comments. Added heartrate percentage high for each attribute.

--- heartrate.py
+++ heartrate.py
@@ -6,7 +6,7 @@ import numpy as np
 import math
 import csv
 
-# Pre-Data (Damien)
+# Pre-Data (Damien) Legger input fra bruker til slutt.
 
 max_heartrate = 188 # Max hjerteslag - hoy intensitet
 resting_heartrate = 52 # Hvilepuls - Generell
@@ -15,20 +15,24 @@ resting_heartrate = 52 # Hvilepuls - Generell
 
 def heartrate_loader(filename):
     records = []
-    with open(filename, 'rb') as csvfile: # Don't overwrite file.
-        csvreader = csv.reader(csvfile, delimiter = ',') # Distinguishes between types
+    with open(filename, 'rb') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter = ',')
         next(csvreader,'none') # Skips header line
         for row in csvreader:
-            if len(row) == 2: # Amount of rows.
+            if len(row) == 3:
                 records.append({
                     "navn": row[0].strip(),
-                    "heartratelow_percentage": float(row[1].strip())
+                    "heartratelow_percentage": float(row[1].strip()),
+                    "heartratehigh_percentage": float(row[2].strip())
                 })
     return records
 
-# Import fil fra mappe
+# csv-data
 heartrate_data = heartrate_loader("Data/heartrate.data")
 
 # For loop to iterate through data from csv-file
 navn = [x['navn'] for x in heartrate_data]
+heartratelow_percentage = [x['heartratelow_percentage'] for x in heartrate_data]
+heartratehigh_percentage = [x['heartratehigh_percentage'] for x in heartrate_data]
 
+print heartratelow_percentage, heartratehigh_percentage
