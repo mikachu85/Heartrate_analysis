4bd6bccfef96e3be759356d8bff32bc4ed23197c
Damien <michelld@hotmail.com>
2015-10-09 16:46:08 +0200

    Added Heart Rate Reserve. Added input for user defined heartrates.

--- heartrate.py
+++ heartrate.py
@@ -6,11 +6,12 @@ import numpy as np
 import math
 import csv
 
-# Pre-Data (Damien) Legger input fra bruker til slutt.
+# Pre-Data (user-input based)
 
-max_heartrate = 188 # Max hjerteslag - hoy intensitet
-resting_heartrate = 52 # Hvilepuls - Generell
-
+max_heartrate = int(input('What is your max heartrate?')) # Max hjerteslag - hoy intensitet
+resting_heartrate = int(input('What is your heartrate when rested?')) # Hvilepuls - Generell
+heartrate_reserve = max_heartrate - resting_heartrate # Heartrate reserve
+print "your heartrate reserve is: ", heartrate_reserve
 # csv - file loader
 
 def heartrate_loader(filename):
@@ -46,10 +47,12 @@ hm2 = heartratehigh_percentage
 hr3 = heartreserve_low
 hr4 = heartreserve_high
 mh = max_heartrate
+hrr = heartrate_reserve
+rhr = resting_heartrate
 
 
 for i in range (0,6):
-    print (mh*hm1[i]), '\t', (mh*hm2[i]), (mh*hr3[i]), '\t', (mh*hr4[i])
+    print (mh*hm1[i]), '\t', (mh*hm2[i]), "\t", (hrr*hr3[i]+rhr), '\t', (hrr*hr4[i]+rhr)
 
 
 
