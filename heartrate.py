3ebf0c6dde33916f62168e2c61cdb7024843047f
Damien <michelld@hotmail.com>
2015-10-13 22:38:29 +0200

    Created racepredictor for 5k, 10k, 21k. The program needs strip for minutes/seconds implementation.

--- heartrate.py
+++ heartrate.py
@@ -4,6 +4,7 @@ __author__ = 'Damien'
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+
 import methods
 
 
@@ -15,8 +16,8 @@ resting_heartrate = 50
 #max_heartrate = int(input('What is your max heartrate?')) # Max hjerteslag - hoy intensitet
 #resting_heartrate = int(input('What is your heartrate when rested?')) # Hvilepuls - Generell
 hrr = max_heartrate - resting_heartrate # Heartrate Reserve
-print "\nYour max, resting and reserve heartrate are: ", max_heartrate, ",", resting_heartrate, ",", hrr, "\n"
-
+print "\nYour max, resting and reserve heartrate are: ", max_heartrate, ",", resting_heartrate, ",", hrr
+print "\nYour heartrate for the desired running types are as follows: \n\n"
 # csv-data
 heartrate_data = methods.heartrate_loader("Data/heartrate.data")
 
@@ -41,7 +42,8 @@ methods.tabledraw()
 for i in range (0,6):
     print '%18s  %1s  %3s %1s %3s %1s %1s %3s %1s %3s %1s' % (navn[i], "[", int(mh*hm1[i]), "-", int(mh*hm2[i]), "]", "[", int(hrr*hr3[i]+rhr),"-", int(hrr*hr4[i]+rhr), "]")
 
-
+# Race predictor
+methods.racepredictor()
 
 
 
--- methods.py
+++ methods.py
@@ -2,6 +2,7 @@ from __future__ import division
 __author__ = 'Damien'
 
 import csv
+import time
 
 # csv - file loader
 def heartrate_loader(filename):
@@ -23,5 +24,15 @@ def heartrate_loader(filename):
 def tabledraw():
     print '%18s' % "Type", '%12s' % "Heart I", '%14s' % "Heart II"
     print "_________________________________________________"
+
+
+def racepredictor():
+    time1k = float(input('\nHow fast can you run 1km? [Type minutes:seconds]'))
+    print '\nThe approximated times for 5k,10k,21k will be:\n'
+    if time1k >= 5 and time1k <= 6:
+        print "5k time: " , time1k*5.3 , "\n\n10k time:" , time1k*10.6 , "\n\n21k time:" , time1k*21.9
+    elif time1k >= 6 and time1k <= 7:
+        print "5k time: " , time1k*5.6 , "\n\n10k time:" , time1k*10.9 , "\n\n21k time:" , time1k*22.5
+
 
 
