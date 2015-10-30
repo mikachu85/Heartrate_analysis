076aabe17113e5f7fa7dda863153548b28c2d027
Damien <michelld@hotmail.com>
2015-10-31 00:49:40 +0100

    Fixed racepredictor for 5k, 10k, and 21k. Values have been corrected with added minutes for further distance. Also added a counter in case minutes exceeded 60 minutes (+1 hour). Correct print statements added for 5k, 10k, and 21k.

--- methods.py
+++ methods.py
@@ -1,10 +1,12 @@
 from __future__ import division
 __author__ = 'Damien'
 
+
 import math
 import csv
 import time
 import random
+
 # csv - file loader
 def heartrate_loader(filename):
     records = []
@@ -22,9 +24,11 @@ def heartrate_loader(filename):
                 })
     return records
 
+
 def tabledraw():
     print '%18s' % "Type", '%12s' % "Heart I", '%14s' % "Heart II"
     print "_________________________________________________"
+    return True
 
 
 def racepredictor():
@@ -35,10 +39,73 @@ def racepredictor():
     calc_0 = int(calc[0])
     calc_1 = int(calc[1])
 
-    time5k_min = math.ceil(calc_0*5.3)
-    print time5k_min
-    time5k_sec = (calc_1 - calc_1) + random.randint(1,59)
-    print "5k time: " , '%00.1s' % time5k_min, ':' , time5k_sec
+    time5k_min = math.ceil(calc_0*5)
+    time10k_min = math.ceil(calc_0*10)
+    time21k_min = math.ceil(calc_0*21)
+    time21k_h = 0
+
+    if calc_1 < 10:
+        time_sec = random.randint(5,20)
+        time5k_min += 1
+        time10k_min += 2
+        time21k_min += 4
+
+    elif calc_1 >= 10 and calc_1 < 30:
+        time_sec = random.randint(20,40)
+        time5k_min += 2
+        time10k_min += 4
+        time21k_min += 14
+
+    elif calc_1 == 30:
+        time_sec = 30
+        time5k_min += 3
+        time10k_min += 6
+        time21k_min += 17
+
+    elif calc_1 >= 30 and calc_1 < 45:
+        time_sec = random.randint(30,59)
+        time5k_min += 4
+        time10k_min += 7
+        time21k_min += 19
+
+    elif calc_1 >= 45:
+        time_sec = random.randint(40,59)
+        time5k_min += 4
+        time10k_min += 10
+        time21k_min += 22
+
+
+    # If person uses more than 60 minutes to complete 21k
+    if time21k_min >= 60 and time21k_min <= 119:
+        time21k_min -= 60
+        time21k_h += 1
+
+    elif time21k_min >= 120 <= 179:
+        time21k_min -= 120
+        time21k_h += 2
+
+    elif time21k_min >= 180:
+        time21k_min -= 180
+        time21k_h += 3
+
+    # Fixing decimals. Removing unwanted zeroes.
+    time5k_min_fix = '%0.d' % time5k_min
+    time10k_min_fix = '%0.d' % time10k_min
+    time21k_min_fix = '%0.d' % time21k_min
+
+    print "5k time: " , time5k_min_fix, 'minutes', ':' , time_sec, 'seconds'
+    print "10k time: " , time10k_min_fix, 'minutes', ':' , time_sec - 2, 'seconds'
+
+    if time21k_h > 1:
+        print "21k time: " , time21k_h, 'hours', ':', time21k_min_fix, 'minutes', ':', time_sec - random.randint(1,3), 'seconds'
+    elif time21k_h == 1:
+        print "21k time: " , time21k_h, 'hour', ':', time21k_min_fix, 'minutes', ':', time_sec - random.randint(1,3), 'seconds'
+    elif time21k_h == 0:
+        print time21k_min_fix, 'minutes', ':', time_sec - random.randint(1,3), 'seconds'
+
+    return True
+
+
 
 
 # def get_sec(s):
